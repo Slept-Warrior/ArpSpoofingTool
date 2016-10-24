7e86cfc1cefd866d338dad1c53544cde0ad9b77b
Slept-Warrior <395533269@qq.com>
2016-10-24 15:23:33 +0800

    Translated
    Replace gibberish with English

--- Sniffer/Main.Designer.cs
+++ Sniffer/Main.Designer.cs
@@ -3,14 +3,14 @@
     partial class Main
     {
         /// <summary>
-        /// 필수 디자이너 변수입니다.
+        /// Required designer variable.(?)
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        /// 사용 중인 모든 리소스를 정리합니다.
+        /// Clean up any resources that used.
         /// </summary>
-        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        /// <param name="disposing">True if a Resource is need to be deleted, false otherwise</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -20,11 +20,10 @@
             base.Dispose(disposing);
         }
 
-        #region Windows Form 디자이너에서 생성한 코드
+        #region Windows Form Genereted Codes
 
         /// <summary>
-        /// 디자이너 지원에 필요한 메서드입니다.
-        /// 이 메서드의 내용을 코드 편집기로 수정하지 마십시오.
+        /// Generated Codes for IDE Support - Do Not Edit
         /// </summary>
         private void InitializeComponent()
         {
